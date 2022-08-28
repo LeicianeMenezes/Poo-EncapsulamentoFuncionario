c1fbd8b4b7e282717afe2f2a513b5afb119b9b7f
Leiciane Menezes <ldsm.ecp19@uea.edu.br>
2022-08-28 01:59:19 -0400

    Primeiro commit

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,68 @@
+package aplicacao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidade.Funcionario;
+
+public class Programa {
+
+   public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		List<Funcionario> list = new ArrayList<>();
+		
+		System.out.print("Quantos funcionarios serão registrados? ");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println();
+			System.out.println("Funcionario #" + i + ": ");
+
+			System.out.print("Id: ");
+			int id = sc.nextInt();
+			while (hasId(list, id)) {
+				System.out.print("Id already taken. Try again: ");
+				id = sc.nextInt();
+			}
+			
+			System.out.print("Nome: ");
+			sc.nextLine();
+			String nome = sc.nextLine();
+			System.out.print("Salario: ");
+			double salario = sc.nextDouble();
+			list.add(new Funcionario(id, nome, salario));
+		}
+		
+		System.out.println();
+		System.out.print("Insira o ID do funcionário que terá aumento salarial: ");
+		int id = sc.nextInt();
+		Funcionario emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		if (emp == null) {
+			System.out.println("Esse id não existe!");
+		}
+		else {
+			System.out.print("Insira a porcentagem: ");
+			double porcentagem = sc.nextDouble();
+			emp.aumentarSalario(porcentagem);
+		}
+		
+		System.out.println();
+		System.out.println("Lista de funcionarios:");
+		for (Funcionario obj : list) {
+			System.out.println(obj);
+		}
+				
+		sc.close(); 
+	}
+	
+	public static boolean hasId(List<Funcionario> list, int id) {
+		Funcionario emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return emp != null;
+	}
+    
+}
